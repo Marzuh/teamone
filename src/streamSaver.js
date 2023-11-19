4c634ea867d0d3c0dec3d1b615a72359ea136034
krsobo <krsobo@taltech.ee>
2023-11-19 18:07:41 +0200

    #6 save file with left join time (not workin file overiting)

--- src/streamSaver.js
+++ src/streamSaver.js
@@ -6,7 +6,7 @@ const { intervalId } = require('./streamScrapping');
 
 // const browserPath = '/usr/bin/google-chrome';
 // const browserPath = 'C:\\program Files\\Google\\Chrome\\Application\\chrome.exe';
-const browserPath = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
+const browserPath = 'C:/Program Files/Google/Chrome/Application/chrome.exe';
 
 const browserAgs = {
   headless: false,
@@ -21,77 +21,77 @@ const timeoutDuration = 0;
 async function chooseMeetingInBrowser(page) {
   try {
     const continueOnBrowserSelector = 'button[data-tid="joinOnWeb"]';
-    logger.debug(`Waiting for "${continueOnBrowserSelector}".`);
+    // logger.debug(`Waiting for "${continueOnBrowserSelector}".`);
     await page.waitForSelector(continueOnBrowserSelector, { timeout: 5000 });
-    logger.debug(`Selector "${continueOnBrowserSelector}" is found.`);
+    // logger.debug(`Selector "${continueOnBrowserSelector}" is found.`);
     await page.click(continueOnBrowserSelector);
   } catch (error) {
-    logger.debug('Error caught for no continueOnBrowserSelector.');
+    // logger.debug('Error caught for no continueOnBrowserSelector.');
   }
 }
 
 async function turnOffCamera(iframeContentFrame) {
   const turnOffCameraSelector = 'div[role="checkbox"][data-tid="toggle-video"]';
-  logger.debug(`Waiting for "${turnOffCameraSelector}" in the iframe content frame.`);
+  // logger.debug(`Waiting for "${turnOffCameraSelector}" in the iframe content frame.`);
   await iframeContentFrame.waitForSelector(turnOffCameraSelector, { timeout: timeoutDuration });
-  logger.debug(`Selector "${turnOffCameraSelector}" found in the iframe content frame.`);
+  // logger.debug(`Selector "${turnOffCameraSelector}" found in the iframe content frame.`);
 
   const cameraState = await iframeContentFrame.evaluate((selector) => {
     const element = document.querySelector(selector);
     return element ? element.getAttribute('data-cid') : null;
   }, turnOffCameraSelector);
-  logger.debug(`Camera current state: ${cameraState}`);
+  // logger.debug(`Camera current state: ${cameraState}`);
 
   if (cameraState === 'toggle-video-true') {
     await iframeContentFrame.click(turnOffCameraSelector);
   } else {
-    logger.debug('Camera is already turned off.');
+    // logger.debug('Camera is already turned off.');
   }
 
   const cameraStateAfter = await iframeContentFrame.evaluate((selector) => {
     const element = document.querySelector(selector);
     return element ? element.getAttribute('data-cid') : null;
   }, turnOffCameraSelector);
-  logger.info(`Camera pre-login state: ${cameraStateAfter}`);
+  // logger.info(`Camera pre-login state: ${cameraStateAfter}`);
 }
 
 async function turnOffMicrophone(iframeContentFrame) {
   const turnOffMicrophoneSelector = 'div[role="checkbox"][data-tid="toggle-mute"]';
-  logger.debug(`Waiting for "${turnOffMicrophoneSelector}" in the iframe content frame.`);
+  // logger.debug(`Waiting for "${turnOffMicrophoneSelector}" in the iframe content frame.`);
   await iframeContentFrame.waitForSelector(turnOffMicrophoneSelector, { timeout: timeoutDuration });
-  logger.debug(`Selector "${turnOffMicrophoneSelector}" found in the iframe content frame.`);
+  // logger.debug(`Selector "${turnOffMicrophoneSelector}" found in the iframe content frame.`);
 
   const microphoneState = await iframeContentFrame.evaluate((selector) => {
     const element = document.querySelector(selector);
     return element ? element.getAttribute('data-cid') : null;
   }, turnOffMicrophoneSelector);
-  logger.debug(`Microphone current state: ${microphoneState}`);
+  // logger.debug(`Microphone current state: ${microphoneState}`);
 
   if (microphoneState === 'toggle-mute-true') {
     await iframeContentFrame.click(turnOffMicrophoneSelector);
   } else {
-    logger.debug('Microphone is already turned off.');
+    // logger.debug('Microphone is already turned off.');
   }
   const microphoneStateAfter = await iframeContentFrame.evaluate((selector) => {
     const element = document.querySelector(selector);
     return element ? element.getAttribute('data-cid') : null;
   }, turnOffMicrophoneSelector);
-  logger.info(`Microphone pre-login state: ${microphoneStateAfter}`);
+  // logger.info(`Microphone pre-login state: ${microphoneStateAfter}`);
 }
 
 async function enterUsername(iframeContentFrame, username) {
   const inputFieldSelector = 'input[data-tid="prejoin-display-name-input"]';
-  logger.debug(`Waiting for "${inputFieldSelector}" in the iframe content frame.`);
+  // logger.debug(`Waiting for "${inputFieldSelector}" in the iframe content frame.`);
   await iframeContentFrame.waitForSelector(inputFieldSelector, { timeout: timeoutDuration });
-  logger.debug(`Selector "${inputFieldSelector}" found in the iframe content frame.`);
+  // logger.debug(`Selector "${inputFieldSelector}" found in the iframe content frame.`);
   await iframeContentFrame.type(inputFieldSelector, username);
 }
 
 async function joinTheMeeting(iframeContentFrame) {
   const joinButton = '#prejoin-join-button';
-  logger.debug(`Waiting for "${joinButton}" in the iframe content frame.`);
+  // logger.debug(`Waiting for "${joinButton}" in the iframe content frame.`);
   await iframeContentFrame.waitForSelector(joinButton, { timeout: timeoutDuration });
-  logger.debug(`Selector "${joinButton}" found in the iframe content frame.`);
+  // logger.debug(`Selector "${joinButton}" found in the iframe content frame.`);
   await iframeContentFrame.click(joinButton);
 }
 
@@ -142,7 +142,7 @@ async function saveStream(url, username) {
   await joinTheMeeting(iframeContentFrame);
   // await closeNoCameraNotification(iframeContentFrame);
 
-  logger.debug('start scrapping');
+  // logger.debug('start scrapping');
   await streamScrapping.streamScrapping(iframeContentFrame, datetime, intervalId);
 
   const stream = await getStream(page, { audio: true, video: true, frameSize: 1000 });
@@ -150,22 +150,22 @@ async function saveStream(url, username) {
   const frameRate = 30;
   const saveDirectoryPath = `C:\\Users\\narti\\studies\\iti0303\\${datetime}.mp4`;
 
-  logger.debug('Recording from %s with %s resolution and %s fps to %s', url, resolution, frameRate, saveDirectoryPath);
+  // logger.debug('Recording from %s with %s resolution and %s fps to %s', url, resolution, frameRate, saveDirectoryPath);
 
   const ffmpeg = exec(`ffmpeg -y -i - -c:v libx264 -c:a aac -s ${resolution} -r ${frameRate} ${saveDirectoryPath}`);
   ffmpeg.stderr.on('data', (chunk) => {
-    logger.debug('stream data event occurs. Chunk: %s', chunk.toString());
+    // logger.debug('stream data event occurs. Chunk: %s', chunk.toString());
   });
 
   stream.on('close', () => {
-    logger.debug('stream close event occurs');
+    // logger.debug('stream close event occurs');
     ffmpeg.stdin.end();
   });
 
   stream.pipe(ffmpeg.stdin);
 
   setTimeout(async () => {
-    logger.debug('stream destroyed by timer');
+    // logger.debug('stream destroyed by timer');
     clearInterval(intervalId);
     stream.destroy();
   }, 1000 * 300);
--- src/streamScrapping.js
+++ src/streamScrapping.js
@@ -1,8 +1,19 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const path = require('path');
+const fs = require('fs');
+const csv = require('csv-parser');
 const logger = require('./logger');
 
 let intervalId;
+const mapObjectKeys = (originalObject) => {
+  const mappedObject = {};
+  for (const key in originalObject) {
+    // Replace spaces with underscores and convert to lowercase
+    const newKey = key.replace(/ /g, '_').toLowerCase();
+    mappedObject[newKey] = originalObject[key];
+  }
+  return mappedObject;
+};
 async function scrapeStream(iframeContentFrame, datetime) {
   // eslint-disable-next-line no-template-curly-in-string
   const directoryPath = 'C:/Users/narti/studies/iti0303/'; // Update the directory path
@@ -10,10 +21,11 @@ async function scrapeStream(iframeContentFrame, datetime) {
   const csvFilePath = path.join(directoryPath, csvFileName); // Combine directory and filename
   const csvWriter = createCsvWriter({
     path: csvFilePath,
-    append: false, // Append records to an existing file
     header: [
-      { id: 'timestamp', title: 'Timestamp' },
-      { id: 'participants', title: 'Participants' },
+      { id: 'meeting_uid', title: 'Meeting UID' },
+      { id: 'participant_name', title: 'Participant Name' },
+      { id: 'joined_timestamp', title: 'Joined Timestamp' },
+      { id: 'left_timestamp', title: 'Left Timestamp' },
     ],
   });
 
@@ -27,10 +39,36 @@ async function scrapeStream(iframeContentFrame, datetime) {
 
   const participantsListElement = await iframeContentFrame.waitForSelector('[data-tid="app-layout-area--end"]');
 
+  let participantsData = [];
+  let participantsNames = [];
+  const readParticipantsFile = async () => {
+    logger.info('start reading');
+    fs.createReadStream(csvFilePath)
+      .pipe(csv())
+      .on('data', (row) => {
+        const isEmptyRow = Object.values(row).every((value) => !value.trim());
+        if (!isEmptyRow) {
+          const mappedRow = mapObjectKeys(row);
+          participantsData.push(mappedRow);
+          participantsNames.push(mappedRow.participant_name);
+          logger.error(JSON.stringify(mappedRow));
+        }
+      })
+      .on('end', () => {
+        // logger.error('CSV file reading completed.');
+        logger.info(JSON.stringify(participantsData));
+        logger.info(JSON.stringify(participantsNames));
+      });
+  };
+
+  let participantsUpdate = false;
   const pollParticipants = async () => {
+    logger.debug(`default data ${participantsData}`);
+    logger.debug(`default names ${participantsNames}`);
+    await readParticipantsFile();
     const currentTimestamp = new Date().toISOString();
     const participants = await participantsListElement.$$('[data-cid="roster-participant"]');
-    const participantNames = [];
+    const currentPartisipantsNames = [];
 
     logger.debug('start scrapping participants names');
     // eslint-disable-next-line no-restricted-syntax
@@ -39,21 +77,53 @@ async function scrapeStream(iframeContentFrame, datetime) {
       const nameElement = await participantElement.$('span');
       // eslint-disable-next-line no-await-in-loop
       const name = await nameElement.evaluate((element) => element.textContent.trim());
-      participantNames.push(name);
+      currentPartisipantsNames.push(name);
+
+      const existingParticipant = participantsData.find(
+        (participant) => participant.participant_name === name,
+      );
+
+      if (!existingParticipant || existingParticipant.left_timestamp !== '') {
+        participantsUpdate = true;
+        participantsData.push({
+          meeting_uid: 2, // Replace with the actual meeting UID when available
+          participant_name: name,
+          joined_timestamp: currentTimestamp,
+          left_timestamp: '', // Initial value is empty
+        });
+        participantsNames.push(name);
+      }
     }
 
-    logger.debug('finish scrapping participants names', participantNames);
+    // Check if there are participants in participantsNames not present in currentPartisipantsNames
+    const missingParticipants = participantsNames.filter(
+      (name) => !currentPartisipantsNames.includes(name),
+    );
 
-    const data = [
-      {
-        timestamp: currentTimestamp,
-        participants: participantNames.join(', '),
-      },
-    ];
+    logger.debug(`missing partisipants: ${JSON.stringify(missingParticipants)}`);
+    // Update left_timestamp for missing participants
+    missingParticipants.forEach((missingName) => {
+      const missingParticipant = participantsData.find(
+        (participant) => participant.participant_name === missingName,
+      );
+      if (missingParticipant && missingParticipant.left_timestamp === '') {
+        participantsUpdate = true;
+        missingParticipant.left_timestamp = currentTimestamp;
+      }
+    });
 
-    logger.debug('write data to file');
-    // Write data as an array of records
-    await csvWriter.writeRecords(data);
+    logger.debug(`participants update ${participantsUpdate}`);
+
+    // Convert the participantsData object values to an array
+    const data = Object.values(participantsData);
+
+    logger.debug(`write data to file ${JSON.stringify(data)}`);
+    // Rewrite file
+    if (participantsUpdate) {
+      await csvWriter.writeRecords(data);
+    }
+    participantsData = [];
+    participantsNames = [];
   };
 
   // Set a timeout to run the initial data collection
@@ -63,7 +133,6 @@ async function scrapeStream(iframeContentFrame, datetime) {
     intervalId = setInterval(pollParticipants, 10000);
   }, 10000);
 }
-
 module.exports = {
   streamScrapping: scrapeStream,
   intervalId, // Export the interval ID
